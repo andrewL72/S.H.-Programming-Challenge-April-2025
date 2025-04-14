9fdaa469629481666fbce7538cb8bddc09fd2669
andrewL72 <andrew.leamy72@gmail.com>
2025-04-14 09:28:12 -0700

    overhauled index.php
    full update to final release version

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 //  April 2025
 //  Andrew Leamy
 
-//
 ?>
 
 <!doctype html>
@@ -24,23 +23,44 @@
 
   <body>
     <h1>H.S. Programming Challenge</h1>
-    <p style="margin-left: 40px"> created by Andrew Leamy </p>
-    <br>
+    <h2> created by Andrew Leamy </h2>
     <br>
     <br>
 
     <div class="container" style="padding-left: 20px">
-      <button type="button" onclick="getClinicianStatus(1)">Fetch Status</button>
-      <br> <br>
-      <p> The current status of Clinician 1 is: <p>
-      <div id="curClinicianStatus"> Not yet fetched. </div>
+      <p>The current status of all Clinicians are:<p>
+      <div id="clinicianTable">
+        <table class='table table-striped'> <thead class='thead-light'> 
+          <tr> <th scope='col'></th> <th scope='col'>Clinician</th> <th scope='col'>Coordinates</th> <th scope='col'>Status</th> <th scope='col'>GeoJSON</th> </tr> </thead>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+          <tr> <td><input type='radio'></td> <td></td> <td></td> <td></td> <td></td> </tr>
+        </table>
+      </div>
+      <br>
+      <p>Clinician locations are polled every 60 seconds.</p>
+      <p>This table automatically refreshes every 60 seconds.</p>
+      <br>
+      <div><button onclick='manualTableUpdate()'>Update Manually</button></div>
     </div>
+
+    <div id="loadingOverlay">
+      <div class="cv-spinner">
+        <span class="spinner"></span>
+      </div>
+    </div>
+
 
     <!-- import boostrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
     <!-- ajax queries -->
     <script>
+      var updateTimeout;
+
       function getClinicianStatus(clinicianID){
         $.ajax({
           url: "./scripts/getAllCliniciansStatus.php",
@@ -50,7 +70,7 @@
           },
           success: function (data){
             console.log("data recieved from getAllCliniciansStatus!");
-            //$("#curClinicianStatus").html(data);
+            $("#clinicianStatus").html(data);
           },
           error:function(e){
                 //the script that runs the curl call to the api failed.
@@ -58,7 +78,6 @@
                 console.error(e);
             }  
         });
-        updateCliniciansTable();
       }
 
       function updateCliniciansTable()
@@ -66,17 +85,35 @@
         $.ajax({
           url: "./scripts/buildCliniciansTable.php",
           type: "GET",
+          beforeSend: function() {
+            $("#loadingOverlay").fadeIn(300);
+          },
           success: function (data){
             console.log("data recieved from buildCliniciansTable!");
-            $("#curClinicianStatus").html(data);
+            $("#clinicianTable").html(data);
           },
           error:function(e){
                 //the script that runs the curl call to the api failed.
                 console.error("buildCliniciansTable.php returned an error.");
                 console.error(e);
-            }  
+            },
+          complete: function(){
+            $("#loadingOverlay").fadeOut(300);
+            updateTimeout = setTimeout(updateCliniciansTable, 65000);
+          }  
         });
       }
+
+      function manualTableUpdate()
+      {
+        clearTimeout(updateTimeout);
+        updateCliniciansTable();
+      }
+
+      $(document).ready(function(){
+        $("#loadingOverlay").fadeIn(300);
+        setTimeout(updateCliniciansTable, 5000);
+      });
     </script>
   </body>
 </html>
