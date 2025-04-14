7290b34124b56d43ee31972816fd76419674ce1a
andrewL72 <andrew.leamy72@gmail.com>
2025-04-14 09:03:18 -0700

    Update runServer.bat

--- runServer.bat
+++ runServer.bat
@@ -1,5 +1,7 @@
 copy ".\data\clinicians default.csv" ".\data\clinicians.csv"
 copy ".\data\event_log default.csv" ".\data\event_log.csv"
-start ".\scripts\clinicianTrackingService.bat"
+cd ".\scripts"
+start clinicianTrackingService.bat
+cd ".."
 start "" http://localhost:8000/index.php
 php -S localhost:8000
